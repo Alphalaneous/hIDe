ce0aacdb52ae05a4b635852c87f195dc7ae43a61
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-07-22 14:57:42 -0400

    Fix weird click on steps incompat

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,27 @@ bool isShiftHeld = false;
 class $modify(MyCCKeyboardDispatcher, CCKeyboardDispatcher) {
 
     bool dispatchKeyboardMSG(enumKeyCodes key, bool down, bool arr) {
-		CCScene* currentScene = CCDirector::sharedDirector()->getRunningScene();
-		EditLevelLayer* layer = typeinfo_cast<EditLevelLayer*>(currentScene->getChildren()->objectAtIndex(0));
-		if(layer){
-			MyEditLevelLayer* myLayer = static_cast<MyEditLevelLayer*>(currentScene->getChildren()->objectAtIndex(0));
-			if (down && (key == KEY_Shift)) {
-				if(!isShiftHeld) {
-					isShiftHeld = true;
-					myLayer->setIDVisible(true);
+
+		Loader::get()->queueInMainThread([key, down, arr]() {
+			if(CCScene* currentScene = CCDirector::sharedDirector()->getRunningScene()) {
+				if(currentScene->getChildrenCount() == 0) return;
+				if(EditLevelLayer* layer = typeinfo_cast<EditLevelLayer*>(currentScene->getChildren()->objectAtIndex(0))){
+					MyEditLevelLayer* myLayer = static_cast<MyEditLevelLayer*>(currentScene->getChildren()->objectAtIndex(0));
+					if (down && (key == KEY_Shift)) {
+						if(!isShiftHeld) {
+							isShiftHeld = true;
+							myLayer->setIDVisible(true);
+						}
+					}
+					if (!down && (key == KEY_Shift)) {
+						if(isShiftHeld){
+							isShiftHeld = false;
+							myLayer->setIDVisible(false);
+						}
+					}
 				}
-				return true;
 			}
-			if (!down && (key == KEY_Shift)) {
-				if(isShiftHeld){
-					isShiftHeld = false;
-					myLayer->setIDVisible(false);
-				}
-				return true;
-			}
-		}
+		});
         return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, arr);
     }
 };
